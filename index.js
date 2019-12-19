01ccff9fbd4792966525b22f0c5ca0a4664a9478
Dominic Tarr <dominic.tarr@gmail.com>
2019-12-20 11:40:01 +1300

    clearer names, remove dead code

--- index.js
+++ index.js
@@ -34,19 +34,15 @@ exports.multibox = function (msg, recipients, max) {
   var key = randombytes(32)
   var onetime = keypair()
 
-  var _key = concat([new Buffer([recipients.length & max]), key])
+  var length_and_key = concat([new Buffer([recipients.length]), key])
   return concat([
     nonce,
     onetime.publicKey,
     concat(recipients.map(function (r_pk, i) {
-      return secretbox(_key, nonce, scalarmult(onetime.secretKey, r_pk))
+      return secretbox(length_and_key, nonce, scalarmult(onetime.secretKey, r_pk))
     })),
     secretbox(msg, nonce, key)
   ])
-}
-
-function get_key(ctxt, my_key) {
-
 }
 
 exports.multibox_open_key = function (ctxt, sk, max) { //, groups...
@@ -56,22 +52,19 @@ exports.multibox_open_key = function (ctxt, sk, max) { //, groups...
   var nonce = ctxt.slice(0, 24)
   var onetime_pk = ctxt.slice(24, 24+32)
   var my_key = scalarmult(sk, onetime_pk)
-  var _key, key, length, start = 24+32, size = 32+1+16
+  var length_and_key, key, length, start = 24+32, size = 32+1+16
   for(var i = 0; i <= max; i++) {
     var s = start+size*i
-    if(s + size > (ctxt.length - 16)) continue
-    _key = secretbox_open(ctxt.slice(s, s + size), nonce, my_key)
-    if(_key) return _key
+    if(s + size > (ctxt.length - 16)) return null
+    length_and_key = secretbox_open(ctxt.slice(s, s + size), nonce, my_key)
+    if(length_and_key) return length_and_key
   }
-
-  if(!key) return
-  return secretbox_open(ctxt.slice(start+length*size), nonce, key)
 }
 
-exports.multibox_open_body = function (ctxt, _key) { //, groups...
-  if(!_key) return
-  var key = _key.slice(1)
-  var length = _key[0]
+exports.multibox_open_body = function (ctxt, length_and_key) { //, groups...
+  if(!length_and_key) return
+  var key = length_and_key.slice(1)
+  var length = length_and_key[0]
   var start = 24+32, size = 32+1+16
   var nonce = ctxt.slice(0, 24)
   return secretbox_open(ctxt.slice(start+length*size), nonce, key)
@@ -82,5 +75,3 @@ exports.multibox_open = function (ctxt, sk, max) { //, groups...
   var _key = exports.multibox_open_key(ctxt, sk, max)
   if(_key) return exports.multibox_open_body(ctxt, _key)
 }
-
-
